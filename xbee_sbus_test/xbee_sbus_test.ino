1ac20042152e735956c6691a49a05e76b7c9df7d
Tuan Dinh Luong <66798302+TL-4319@users.noreply.github.com>
2023-07-13 19:47:16 -0500

    Create xbee_sbus_test.ino

---
+++ xbee_sbus_test/xbee_sbus_test.ino
@@ -0,0 +1,28 @@
+#include "sbus.h"
+
+/* SBUS object, reading SBUS */
+bfs::SbusRx sbus_rx(&Serial1, false);
+
+/* SBUS data */
+bfs::SbusData data;
+
+void setup() {
+  /* Serial to display data */
+  Serial.begin(115200);
+  while (!Serial) {}
+  /* Begin the SBUS communication */
+  sbus_rx.Begin();
+}
+
+void loop () {
+  if (sbus_rx.Read()) {
+    /* Grab the received data */
+    data = sbus_rx.data();
+    /* Display the received data */
+    for (int8_t i = 0; i < data.NUM_CH; i++) {
+      Serial.print(data.ch[i]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+}
