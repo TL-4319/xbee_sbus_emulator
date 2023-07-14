42a59b823908f001c4c135878a7a2a90b2be5989
Tuan Dinh Luong <66798302+TL-4319@users.noreply.github.com>
2023-07-13 19:45:50 -0500

    Create sbus_frame_generator.ino

---
+++ sbus_frame_generator/sbus_frame_generator.ino
@@ -0,0 +1,41 @@
+#include "sbus.h"
+bfs::SbusTx sbus_tx(&Serial1, false);
+bfs::SbusData data;
+
+void setup(){
+  Serial.begin(115200);
+  while(!Serial){}
+  sbus_tx.Begin();
+  data.lost_frame = false;
+  data.failsafe = false;
+  data.ch17 = false;
+  data.ch18 = false;
+  data.ch[0] = 991;
+  data.ch[1] = 172;
+  data.ch[2] = 172;
+  data.ch[3] = 172;
+  data.ch[4] = 172;
+  data.ch[5] = 172;
+  data.ch[6] = 172;
+  data.ch[7] = 172;
+  data.ch[8] = 172;
+  data.ch[9] = 172;
+  data.ch[10] = 172;
+  data.ch[11] = 172;
+  data.ch[12] = 172;
+  data.ch[13] = 172;
+  data.ch[14] = 172;
+  data.ch[15] = 172;
+  sbus_tx.data(data);
+  sbus_tx.Write();
+  for (uint8_t i = 0; i < 25; i++){
+    if (sbus_tx.buf_[i] < 0x10){Serial.print("0");}
+    Serial.print(sbus_tx.buf_[i],HEX);
+    Serial.print(" ");
+  }
+}
+
+void loop() {
+  sbus_tx.Write();
+  delay(1000);
+}
